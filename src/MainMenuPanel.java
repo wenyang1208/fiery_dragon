ad943f6cc7ae74bbc01e7195a514fe479fa6be0c
jackern <jche0387@student.monash.edu>
2024-04-26 22:17:22 +0800

    Button implemented in main menu but button function is not implemented yet

--- src/MainMenuPanel.java
+++ src/MainMenuPanel.java
@@ -1,8 +1,10 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenuPanel extends JPanel implements Runnable {
-    public static final int WIDTH = 1100;
+    public static final int WIDTH = 1200;
     public static final int HEIGHT = 800;
     private ImageIcon backgroundImage;
 
@@ -10,6 +12,54 @@ public class MainMenuPanel extends JPanel implements Runnable {
         backgroundImage = new ImageIcon(getClass().getResource("/background/Fiery.png"));
         backgroundImage = new ImageIcon(backgroundImage.getImage().getScaledInstance(WIDTH, HEIGHT, Image.SCALE_DEFAULT));
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
+
+        setLayout(null);
+
+        JButton startGameButton = new JButton("");
+        JButton gameRuleButton = new JButton("");
+        JButton exitButton = new JButton("");
+
+
+        startGameButton.setBounds(520, 300, 220, 100);
+        gameRuleButton.setBounds(480, 440, 300, 100);
+        exitButton.setBounds(515, 580, 205, 100);
+
+        startGameButton.setOpaque(false);
+        startGameButton.setContentAreaFilled(false);
+        startGameButton.setBorderPainted(false);
+
+        gameRuleButton.setOpaque(false);
+        gameRuleButton.setContentAreaFilled(false);
+        gameRuleButton.setBorderPainted(false);
+
+        exitButton.setOpaque(false);
+        exitButton.setContentAreaFilled(false);
+        exitButton.setBorderPainted(false);
+
+        startGameButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Start Game button clicked");
+            }
+        });
+
+        gameRuleButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Game rule button clicked");
+            }
+        });
+
+        exitButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Exit button clicked");
+            }
+        });
+
+        add(startGameButton);
+        add(gameRuleButton);
+        add(exitButton);
     }
 
     public void paintComponent(Graphics g){
