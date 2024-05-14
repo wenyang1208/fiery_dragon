363bd3984f454e4b7f262e00c111defea1a0cbb7
jetxshen <127771410+jetxshen@users.noreply.github.com>
2024-05-14 15:05:51 +0800

    Created a Home class and redirect Application file to Home, representing the home page before entering the game.
    Former-commit-id: 9544558db40e2f3873d9fa6e286b01196fffec26

--- src/Game/Application.java
+++ src/Game/Application.java
@@ -1,11 +1,27 @@
 package Game;
 
-import Game.Game;
+import javax.swing.JFrame;
 
 public class Application {
+    public static final int frameWidth = 1050;
+    public static final int frameHeight = 750;
 
     public static void main(String[] args) throws Exception{
-        Game g = new Game();
+        JFrame window = new JFrame("Fiery Dragon");
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setSize(frameWidth,frameHeight);
+        Home mmp = new Home(window);
+        window.add(mmp);
+//    window.pack();
+
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
+//    Home h =
+//    new Home();
+//    Game g = new Game();
     }
+
 }
+
 
