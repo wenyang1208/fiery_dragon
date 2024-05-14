e99e61be4a3208b28f7c10365e0ad6c6ae949da6
wenyang-eng <wchu0033@student.monash.edu>
2024-05-14 18:24:22 +0800

    Amend the ChitCardController class for initialise the setup of each chit card.
    Former-commit-id: fb7ce4ad46e9e390fca065ea3cc6b3a5368f5787

--- src/Controller/ChitCardController.java
+++ src/Controller/ChitCardController.java
@@ -1,53 +1,52 @@
 package Controller;
 
-import Game.GameSetup;
-import GameBoardComponent.ChitCard;
+import Game.GamePanel;
 import GameBoardComponent.Deck;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import javax.swing.JLabel;
+
 /**
  * The ChitCardController class manages the functionality related to chit cards in the game.
  * It implements the MouseListener interface to handle mouse events on chit card images.
  */
-public class ChitCardController implements MouseListener {
+public class ChitCardController{
   private Deck deck;
-  private GameSetup chitCardView;
+  private GamePanel chitCardView;
   private Map<JLabel, Integer> labelIndexMap;
   private JLabel chitCardImage;
-  private ArrayList<ChitCard> flippedChitCards;
   /**
    * Constructs a new ChitCardController with default settings.
    */
-  public ChitCardController(){
-    this.chitCardView = new GameSetup();
+  public ChitCardController(GamePanel gamePanel){
+    this.chitCardView = gamePanel;
     this.labelIndexMap = new HashMap<>();
-    this.flippedChitCards = new ArrayList<>();
   }
   /**
    * Initializes the setup of chit cards.
    */
   public void initialiseChitCardSetup(){
     deck = new Deck();
-    deck.startGame();
+    deck.createFullDeck();
     for (int i = 0; i < deck.getChitCards().size(); i++) {
       chitCardImage = new JLabel();
-      chitCardImage.addMouseListener(this);
       deck.getChitCards().get(i).setIsFlipped(false);
       chitCardImage.setIcon(deck.getChitCards().get(i).getImage());
       chitCardView.getChitCardPanel().add(chitCardImage);
       labelIndexMap.put(chitCardImage, i);
     }
   }
+
+  public Map<JLabel, Integer> getLabelIndexMap() {
+    return labelIndexMap;
+  }
+
   /**
    * Retrieves the GameSetup object associated with this ChitCardController.
    *
    * @return The GameSetup object associated with this ChitCardController.
    */
-  public GameSetup getGameSetup(){
+  public GamePanel getGameSetup(){
     return chitCardView;
   }
   /**
@@ -55,34 +54,7 @@ public class ChitCardController implements MouseListener {
    *
    * @return The list of flipped chit cards.
    */
-  public ArrayList<ChitCard> getFlippedChitCards(){
-    return this.flippedChitCards;
-  }
-
-  @Override
-  public void mouseClicked(MouseEvent e) {
-    JLabel clickedLabel = (JLabel) e.getSource();
-    int index = labelIndexMap.get(clickedLabel);
-    clickedLabel.setIcon(deck.getChitCards().get(index).getImage());
-  }
-
-  @Override
-  public void mousePressed(MouseEvent e) {
-
-  }
-
-  @Override
-  public void mouseReleased(MouseEvent e) {
-
-  }
-
-  @Override
-  public void mouseEntered(MouseEvent e) {
-
-  }
-
-  @Override
-  public void mouseExited(MouseEvent e) {
-
+  public Deck getDeck(){
+    return this.deck;
   }
 }
