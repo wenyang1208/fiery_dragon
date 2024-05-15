c114527dd4d7041282eb0273135eccde1f097aea
jetxshen <127771410+jetxshen@users.noreply.github.com>
2024-05-15 15:40:08 +0800

    Created getter and setter methods for width and height of VolcanoCards. Along with methods to add and remove player tokens from each volcano card place
    Former-commit-id: d020dc370a558a438fa26fdead6b3911235a4792

--- src/GameBoardComponent/VolcanoCard.java
+++ src/GameBoardComponent/VolcanoCard.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Image;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 /**
  * The VolcanoCard class represents a volcano card on the game board.
@@ -12,21 +13,26 @@ import javax.swing.JPanel;
 public class VolcanoCard extends JPanel implements Path {
 
   private boolean isOccupied;
-
+  private int width;
+  private int height;
   private Animal animal;
   private int position;
   private Image volcanoCardImage;
+
   /**
    * Constructs a new VolcanoCard object with the specified animal and position.
    *
    * @param animal The animal associated with the volcano card.
    * @param position The position of the volcano card.
    */
-  public VolcanoCard(Animal animal, int position){
+  public VolcanoCard(Animal animal, int position, int width, int height){
     setAnimal(animal);
     setPosition(position);
     setBackground(Color.BLACK);
+    setWidth(width);
+    setHeight(height);
   }
+
   /**
    * Sets the animal associated with the volcano card.
    *
@@ -35,6 +41,7 @@ public class VolcanoCard extends JPanel implements Path {
   public void setAnimal(Animal animal){
     this.animal = animal;
   }
+
   /**
    * Retrieves the animal associated with the volcano card.
    *
@@ -43,6 +50,7 @@ public class VolcanoCard extends JPanel implements Path {
   public Animal getAnimal(){
     return this.animal;
   }
+
   /**
    * Sets the position of the volcano card.
    *
@@ -51,6 +59,7 @@ public class VolcanoCard extends JPanel implements Path {
   public void setPosition(int position) {
     this.position = position;
   }
+
   /**
    * Retrieves the position of the volcano card.
    *
@@ -70,15 +79,46 @@ public class VolcanoCard extends JPanel implements Path {
     super.paintComponent(g);
     //getClass().getClassLoader().getResource("Project_cartoon/Background.jpg")
     volcanoCardImage = new ImageIcon(getClass().getClassLoader().getResource(
-        "VolcanoCardImage/" + getAnimal().getName() + "_volcano_card.png")).getImage();
-    g.drawImage(volcanoCardImage, 0, 0,70,70, null);
+            "VolcanoCardImage/" + getAnimal().getName() + "_volcano_card.png")).getImage();
+    g.drawImage(volcanoCardImage, 0, 0,getWidth(),getHeight(), null);
   }
+
   /**
    * Adds a token to the volcano card.
    *
    * @param token The token to add.
    */
   public void addToken(Token token){
+    token.setCurrentSqaure(this);
+    this.add(new JLabel("", token.getImage(), JLabel.CENTER));
+    revalidate();
+  }
+
+  public void removeToken(){
+    removeAll();
+    revalidate();
+    repaint();
+  }
+
+  public boolean isOccupied() {
+    if(this.getComponents().length != 0){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+
+  public void setWidth(int width){
+    this.width = width;
+  }
+  public int getWidth(){
+    return this.width;
+  }
+  public void setHeight(int height){
+    this.height = height;
+  }
+  public int getHeight(){
+    return this.height;
   }
 }
-
