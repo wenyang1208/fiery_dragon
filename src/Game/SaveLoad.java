a8b99615afbd4e5e02d2e701f41afb6bd9e61ae7
wenyang-eng <wchu0033@student.monash.edu>
2024-06-04 10:18:08 +0800

    Try implement save with json format
    Former-commit-id: 0455d26633307897d6cdcc534fe47fdf5ca6f89f

---
+++ src/Game/SaveLoad.java
@@ -0,0 +1,21 @@
+package Game;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.FileOutputStream;
+import java.io.Serializable;
+
+public class SaveLoad {
+
+  public static void save(Serializable data, String fileName){
+    try{
+      FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+      ObjectMapper mapper = new ObjectMapper();
+      String jsonStr = mapper.writerWithDefaultPrettyPrinter().writeValueAsString(data);
+      fileOutputStream.write(jsonStr.getBytes());
+      System.out.println("saved");
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+}
