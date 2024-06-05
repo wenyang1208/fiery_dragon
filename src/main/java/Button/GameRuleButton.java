a6773904b35f32c81911a33059f4f0ed8ad44445
jetxshen <jcho0161@student.monash.edu>
2024-06-05 17:30:00 +0800

    Added new rule of new dragon card in the GameRuleButton
    Former-commit-id: 68572a04cffee498aaf239808cfd7633781bc6c3

--- src/main/java/Button/GameRuleButton.java
+++ src/main/java/Button/GameRuleButton.java
@@ -33,9 +33,10 @@ public class GameRuleButton extends JButton implements ActionListener {
                 "2. Uncover a dragon card. If it matches the animal on your square, move your dragon based on the number of animals on card.\n" +
                 "3. If the card does not match the animal on your square, your turn ends, and your dragon stays put.\n" +
                 "4. Beware of dragon pirates! Move your dragon back the number of spaces shown on the card if you uncover it.\n" +
-                "5. Your dragon can't move if it's in its cave or if the square it would move to is already occupied by other player.\n" +
-                "6. You must land on your cave with exact steps. If you pass it, your turn ends and your dragon stays put.\n" +
-                "7. The game ends when a dragon reaches its cave after going around the volcano clockwise. He or she wins!\n";
+                "5. Beware of the new dragons! Move your dragon back until it reaches an empty cave (any player) and move into that cave.\n" +
+                "6. Your dragon can't move if it's in its cave or if the square it would move to is already occupied by other player.\n" +
+                "7. You must land on your cave with exact steps. If you pass it, your turn ends and your dragon stays put.\n" +
+                "8. The game ends when a dragon reaches its cave after going around the volcano clockwise. He or she wins!\n";
 
         // Show the message dialog
         JOptionPane.showMessageDialog(null, gameRulesMessage, "Game Rules", JOptionPane.INFORMATION_MESSAGE);
