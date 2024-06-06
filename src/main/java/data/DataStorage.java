06b5c7decaad80a85a3ab9f15d3b0ce3ed9843ec
jackern <jche0387@student.monash.edu>
2024-06-06 23:54:18 +0800

    added javadoc
    Former-commit-id: 96af52e98dc07ce0f87d254c001405439edc3652

--- src/main/java/data/DataStorage.java
+++ src/main/java/data/DataStorage.java
@@ -6,6 +6,11 @@ import Controller.VolcanoCardController;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * The DataStorage class is a container for storing the game state.
+ * It is used to save and load game data in a serialized form.
+ * This class holds the necessary data required to restore the game state.
+ */
 public class DataStorage implements Serializable {
     ArrayList<String> playerList;
     int timeLimit;
--- src/main/java/data/SaveLoad.java
+++ src/main/java/data/SaveLoad.java
@@ -4,13 +4,27 @@ import Game.Game;
 
 import java.io.*;
 
+/**
+ * The SaveLoad class is responsible for saving and loading the state of the game.
+ * It uses Java's serialization mechanism to write and read the game state to and from a file.
+ * This class helps in persisting the game data between sessions.
+ */
 public class SaveLoad implements Serializable{
     private Game game;
 
+    /**
+     * Constructs a SaveLoad instance with the specified game.
+     *
+     * @param game the game instance whose state needs to be saved or loaded.
+     */
     public SaveLoad(Game game){
         this.game = game;
     }
 
+    /**
+     * Saves the current state of the game to a binary file.
+     * It serializes the game state into a DataStorage object and writes it to "game_save.dat".
+     */
     public void save() {
         try {
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(new File("game_save.dat")));
@@ -27,6 +41,10 @@ public class SaveLoad implements Serializable{
         }
     }
 
+    /**
+     * Loads the game state from a binary file.
+     * It reads a DataStorage object from "game_save.dat" and restores the game state.
+     */
     public void load() {
         try {
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(new File("game_save.dat")));
